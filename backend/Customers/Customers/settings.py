a0205ac8e8fc80b899af36a3f95b3ef855ed23f9
Matias <rojascofrem@gmail.com>
2025-07-11 12:40:43 -0400

    Make commit

--- backend/Customers/Customers/settings.py
+++ backend/Customers/Customers/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,13 +101,22 @@ WSGI_APPLICATION = 'Customers.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        # Usa la URL de la base de datos de Render, o SQLite si no la encuentra
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
-}
+#Modificación del archivo para que seleccione la base de dato que este definida
+DATABASE_URL = os.environ.get('DATABASE_URL')   
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
